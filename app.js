193f523a572728709a55f2dfd0a22e23694866d2
duydat246 <duydat246@gmail.com>
2021-11-08 21:19:26 +0700

    color flipper

---
+++ app.js
@@ -0,0 +1,17 @@
+const colors = ["green", "yellow", "orange", "red", "blue", "white", "grey"];
+
+const btn = document.getElementById("btn");
+
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+    // get random number
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
